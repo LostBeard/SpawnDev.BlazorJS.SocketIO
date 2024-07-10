69c95359dc45c491b380156b1437b39f872823db
Todd Tanner <todd@spawndev.com>
2024-07-10 19:20:04 -0400

    Demo Program.cs update

--- SpawnDev.BlazorJS.SocketIO.Demo/Program.cs
+++ SpawnDev.BlazorJS.SocketIO.Demo/Program.cs
@@ -5,17 +5,19 @@ using SpawnDev.BlazorJS.SocketIO;
 using SpawnDev.BlazorJS.SocketIO.Demo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddBlazorJSRuntime(out var JS);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Load the SimplePeer Javascript library. Can be loaded using a <script> tag in the index.html instead
+// init SpawnDev.BlazorJS.BlazorJSRuntime service
+builder.Services.AddBlazorJSRuntime();
+
+// Load the Socket.IO Javascript library. Can be loaded using a <script> tag in the index.html instead
 await Socket.Init();
 // A Socket can be created when needed.
-// Here we are creating it at startup and registering it as a service (not required.)
+// Here we are creating it at startup and registering it as a service
 var socket = new Socket("http://localhost:3000");
-JS.Set("socket", socket);
 builder.Services.AddSingleton(socket);
+// example of listening for a message
 socket.On<string>("welcome", welcomeMessage =>
 {
     Console.WriteLine($"Welcome received: {welcomeMessage}");
@@ -23,4 +25,4 @@ socket.On<string>("welcome", welcomeMessage =>
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().BlazorJSRunAsync();
+await builder.Build().RunAsync();
